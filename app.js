e9d2aae519a10c81067fe15d4115b166173813f9
neelansh27 <neelanshsharma30@gmail.com>
2024-02-29 22:05:57 +0530

    Add base layout

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const catalogRouter = require("./routes/catalog");
 const wiki=require('./wiki')
 const app = express();
 
@@ -21,9 +22,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/wiki', wiki);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
